f0c01f810878593f3a3bb2bdb3a86812555fdf30
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-11-19 14:35:23 +0100

    added utilClass and changed routes

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,6 +1,6 @@
 import express from "express"
-import fs from 'fs/promises'
-import User from '../models/user.js'
+
+import {loadUsers} from '../services/fileReaders.js'
 
 const loginRouter = express.Router()
 
@@ -30,22 +30,5 @@ loginRouter.post("/", async (request, response) => {
         response.sendStatus(404)
     }
 })
-
-export async function loadUsers() {
-    const data = await fs.readFile("./users.json", 'utf-8')
-    const usersJson = JSON.parse(data)
-    const usersArray = usersJson.users
-
-    const users = usersArray.map(user =>
-        new User(
-            user.id,
-            user.username,
-            user.password,
-            user.dateOfCreation,
-            user.userLevel
-        )
-    );
-    return users
-}
 
 export default loginRouter
---
+++ services/fileReaders.js
@@ -0,0 +1,68 @@
+import fs from 'fs/promises'
+
+import Chat from '../models/chat.js'
+import Message from '../models/message.js'
+import User from '../models/user.js'
+
+
+//USERS
+export async function loadUsers() {
+    const data = await fs.readFile("./users.json", 'utf-8')
+    const usersJson = JSON.parse(data)
+    const usersArray = usersJson.users
+
+    const users = usersArray.map(user =>
+        new User(
+            user.id,
+            user.username,
+            user.password,
+            user.dateOfCreation,
+            user.userLevel
+        )
+    );
+    return users
+}
+
+//CHATS
+export async function loadChats() {
+    const data = await fs.readFile("./chats.json", 'utf-8')
+    const chatJson = JSON.parse(data)
+    const chatArray = chatJson.chats    
+
+    const chats = chatArray.map(chat =>{
+        const messages = []
+
+        chat.messages.forEach(message => {
+            messages.push(new Message(
+                message.id,
+                message.createdByUser,
+                message.dateOfCreation,
+                message.postedToChat,
+                message.text
+            ))
+        })
+
+        return new Chat(
+            chat.id,
+            chat.name,
+            chat.dateOfCreation,
+            chat.createdByUser,
+            messages
+        )
+    }
+    );
+    return chats
+}
+
+
+export async function loadMessages(chats) {
+    const messages = []    
+
+    chats.forEach(chat => {
+        chat.messages.forEach(message =>{
+            messages.push(message)
+        })
+    });
+
+    return messages
+}
