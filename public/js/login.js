7df6f77e2fa6c5998492285dead371f898d2020e
Jonas-Petersen <jonasvm6@hotmail.com>
2025-11-19 11:17:24 +0100

    Login functionality

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+const loginButton = document.getElementById('loginButton')
+const usernameInput = document.getElementById('usernameInput')
+const passwordInput = document.getElementById('passwordInput')
+
+
+loginButton.addEventListener('click', async () => {
+    console.log('login button clicked');
+    const username = usernameInput.value
+    const password = passwordInput.value
+    try {
+
+        const user = await post(username, password)
+
+    } catch (error) {
+        console.log('Error');
+    }
+})
+
+
+async function post(username, password) {
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    if (response.status === 200) {
+        window.location.href = "/"
+    }
+}
