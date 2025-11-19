568e26635077faedb3bb06988269c01ef2d8c8fd
emilnordstroem <emilstoeve@outlook.com>
2025-11-19 09:05:45 +0100

    models package added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.send('Hello')
 });
 
 app.use("/home", router)
