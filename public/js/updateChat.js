0dd26699351f77abce9e80a58aecdf44c7b07301
emilnordstroem <emilstoeve@outlook.com>
2025-11-22 13:30:53 +0100

    chat name can be updated

---
+++ public/js/updateChat.js
@@ -0,0 +1,23 @@
+const updateChatButton = document.getElementById("updateChatButton")
+
+updateChatButton.addEventListener('click', async () => {
+    const chatNameLabel = document.getElementById('chatNameLabel')
+    const updatedChatName = chatNameLabel.value    
+
+    const chatId = updateChatButton.getAttribute('data-chatId')
+
+    const status = await put(chatId, updatedChatName) 
+
+    window.location.href = `/chats/${chatId}`
+})
+
+async function put (chatId, updatedChatName) {
+    const response = await fetch(`/chats/${chatId}`, 
+        {
+            method: 'PUT', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({updatedChatName: updatedChatName})
+        }
+    )
+    return response
+}
